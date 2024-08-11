31c011b439824a4b159012ab13bfa3390aca1f98
yuanzui_cf <garssblogstu@gmail.com>
2024-08-11 16:35:54 +0800

    feat: Initial Appbar layout

--- lib/src/ui/component/appbar/main.dart
+++ lib/src/ui/component/appbar/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:grassh_renew/src/ui/component/appbar/window_button.dart';
 import 'package:window_manager/window_manager.dart';
 
 class AppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -6,15 +8,27 @@ class AppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DragToMoveArea(
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.black,
+    return const DragToMoveArea(
+      child: SizedBox(
+        height: 30,
+        child: Row(
+          children: [
+            SizedBox(width: 10),
+            Text(
+              "GrassH",
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            WindowButton(),
+          ],
         ),
       ),
     );
   }
 
   @override
-  Size get preferredSize => const Size(0, 25);
+  Size get preferredSize => const Size(0, 30);
 }
---
+++ lib/src/ui/component/appbar/window_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WindowButton extends StatefulWidget {
+  const WindowButton({super.key});
+
+  @override
+  State<WindowButton> createState() => _WindowButtonState();
+}
+
+class _WindowButtonState extends State<WindowButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
