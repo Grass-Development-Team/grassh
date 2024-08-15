e28678bae626d28850abca91e39f778405a346ef
yuanzui-cf <grassblogstu@gmail.com>
2024-08-15 11:26:56 +0800

    feat: Add SideBarTab component

---
+++ lib/src/ui/component/sidebar/tab.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SideBarTab extends StatefulWidget {
+  final void Function()? callback;
+  final IconData? icon;
+  final String? text;
+
+  const SideBarTab({super.key, this.callback, this.icon, this.text});
+
+  @override
+  State<SideBarTab> createState() => _SideBarTabState();
+}
+
+class _SideBarTabState extends State<SideBarTab> {
+  bool _isHover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+        child: GestureDetector(
+          onTap: () {
+            widget.callback?.call();
+          },
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            onEnter: (e) {
+              _isHover = true;
+              setState(() {});
+            },
+            onExit: (e) {
+              _isHover = false;
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeInOut,
+              width: MediaQuery.of(context).size.width,
+              height: 30,
+              decoration: BoxDecoration(
+                color: _isHover ? Theme.of(context).highlightColor : null,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    widget.icon,
+                    size: 10,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
