9283229f59838c18cbc12788ba78fbd739f8663f
yuanzui-cf <grassblogstu@gmail.com>
2024-08-15 11:26:42 +0800

    feat: Add sidebar component

--- lib/src/ui/component/sidebar/main.dart
+++ lib/src/ui/component/sidebar/main.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 
 class SideBar extends StatelessWidget {
   final Widget? child;
+  final double width;
   const SideBar({
     super.key,
+    required this.width,
     this.child,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50,
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.fastEaseInToSlowEaseOut,
+      width: width,
+      child: Center(
+        child: child,
+      ),
     );
   }
 }
